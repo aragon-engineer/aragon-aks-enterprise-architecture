84871beb68821f8896a36f3f1b82f0489460a3dc
José M Aragón <leugimos@protonmail.com>
2025-12-02 13:21:33 -0600

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+/terraform
+    main.tf
+    variables.tf
+    outputs.tf
+    providers.tf
+    networking.tf
+    aks.tf
+    rbac.tf
+
+/devops
+    azure-pipelines.yml
+    stages/
+        build.yml
+        validate.yml
+        deploy.yml
+
+/diagrams
+    aks-architecture.drawio
+
+/scripts
+    bootstrap.sh
+    deploy.ps1
+
+README.md
